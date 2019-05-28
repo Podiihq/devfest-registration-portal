00e958b9fca555b6d2f3e0d00fbb4bb3010242a8
okothkongo1 <okothkongo@gmail.com>
2019-05-28 15:38:35 +0300

    setup credo

--- mix.exs
+++ mix.exs
@@ -42,7 +42,8 @@ defmodule DevfestRegistrationPortal.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false}
     ]
   end
 
@@ -56,7 +57,12 @@ defmodule DevfestRegistrationPortal.MixProject do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate", "test"],
+      check_lint: [
+        "compile --warnings-as-errors --force",
+        "credo --strict",
+        "format --check-formatted"
+      ]
     ]
   end
 end
