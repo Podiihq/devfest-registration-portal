19ae4a2f029343548959c4a2578b13b44fb419a4
Tee22 <traceyOnim22@gmail.com>
2019-07-25 19:16:09 +0300

    added test for codelab challenge

---
+++ test/devfest_registration_portal/codelab_challenge_test.exs
@@ -0,0 +1,17 @@
+defmodule DevfestRegistrationPortal.CodelabChallengeTest do
+  @moduledoc """
+    Handles tests for codelab categories
+  """
+  use DevfestRegistrationPortalWeb.ConnCase
+  alias DevfestRegistrationPortal.Codelabs
+
+  test "create_codelab/1 inserts codelab challenge to database" do
+    assert {:ok, _codelab} =
+             Codelabs.create_codelab(%{
+               "category_id" => 1,
+               "url" => "https://regex101.com",
+               "level" => "level1",
+               "name" => "challenge1"
+             })
+  end
+end
