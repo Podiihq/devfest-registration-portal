dd03b5f8af4b8c238a8fa060f273eed0e4914042
Tee22 <traceyOnim22@gmail.com>
2019-09-02 16:19:03 +0300

    added test for validating description length

--- test/devfest_registration_portal/codelab_challenge_test.exs
+++ test/devfest_registration_portal/codelab_challenge_test.exs
@@ -2,7 +2,8 @@ defmodule DevfestRegistrationPortal.CodelabChallengeTest do
   @moduledoc """
     Handles tests for codelab categories
   """
-  use DevfestRegistrationPortalWeb.ConnCase
+  use DevfestRegistrationPortal.DataCase
+  alias DevfestRegistrationPortal.Codelab
   alias DevfestRegistrationPortal.Codelabs
 
   setup do
@@ -17,6 +18,17 @@ defmodule DevfestRegistrationPortal.CodelabChallengeTest do
   end
 
   test "create_codelab/1 inserts codelab challenge to database", %{valid_attr: valid_attr} do
-    assert {:ok, _codelab} = Codelabs.create_codelab(valid_attr)
+    {:ok, codelab} = Codelabs.create_codelab(valid_attr)
+    assert codelab.name == "challenge1"
+    assert codelab.level == "level1"
+  end
+
+  test "description should be of maximum length of 100", %{valid_attr: valid_attr} do
+    description = String.duplicate("a", 101)
+    attr = %{valid_attr | "description" => description}
+    changeset = Codelab.changeset(%Codelab{}, attr)
+
+    assert %{description: ["should be at most 100 character(s)"]} = errors_on(changeset)
+    assert {:error, _changeset} = Codelabs.create_codelab(attr)
   end
 end
