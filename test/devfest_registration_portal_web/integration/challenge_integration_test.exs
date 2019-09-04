b8a5f9e786e1ce0d0ac3dddbf7a122a7ebe3e485
Tee22 <traceyOnim22@gmail.com>
2019-09-04 15:07:57 +0300

    integration test

--- test/devfest_registration_portal_web/integration/challenge_integration_test.exs
+++ test/devfest_registration_portal_web/integration/challenge_integration_test.exs
@@ -1,3 +1,26 @@
 defmodule DevfestRegistrationPortalTest.ChallengeIntegrationTest do
   use DevfestRegistrationPortalWeb.ConnCase
+  import DevfestRegistrationPortalTest.Helper
+
+  test "admin can create codelab challenge", %{conn: conn} do
+    conn
+    |> login()
+    |> get(Routes.challenge_path(conn, :new))
+    |> follow_form(%{
+      codelab: %{
+        name: "challenge1",
+        url: "https://regex101.com",
+        level: "level1",
+        description: "description1"
+      }
+    })
+    |> assert_response(html: "challenge1 created!")
+  end
+
+  test "admin can list codelab challenge", %{conn: conn} do
+    conn
+    |> login()
+    |> get(Routes.challenge_path(conn, :index))
+    |> assert_response(html: "List Of Codelabs")
+  end
 end
